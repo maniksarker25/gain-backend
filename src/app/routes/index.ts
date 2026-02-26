f46b590827d75c7986a7b0dc53dbdc10288a9012
manik <maniksarker652@gmail.com>
2026-02-26 08:36:38 +0600

    setup routes for student endpoints

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,22 @@
-import express from "express";
-import { userRoutes } from "../modules/user/user.routes";
+import express from 'express';
+import { authRoutes } from '../modules/auth/auth.routes';
+import { studentRoutes } from '../modules/student/student.route';
+import { userRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: "/user",
+    path: '/user',
     route: userRoutes,
+  },
+  {
+    path: '/auth',
+    route: authRoutes,
+  },
+  {
+    path: '/student',
+    route: studentRoutes,
   },
 ];
 
