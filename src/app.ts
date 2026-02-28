417dcc85d590d6c748b55d6b8ff53e166c56a379
manik <maniksarker652@gmail.com>
2026-02-28 14:34:00 +0600

    make some changes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import cors from "cors";
-import express, { Application, Request, Response, urlencoded } from "express";
-import globalErrorHandler from "./app/middlewares/globalErrorHandler";
-import notFound from "./app/middlewares/notFound";
-import router from "./app/routes";
+import cors from 'cors';
+import express, { Application, Request, Response, urlencoded } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
@@ -22,11 +22,11 @@ app.use(
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("NuCoach server is available");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Gain server is available');
 });
 
-app.use("/api/v1", router);
+app.use('/api/v1', router);
 app.use(globalErrorHandler);
 app.use(notFound);
 
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-import { Server } from "http";
-import app from "./app";
-import config from "./app/config";
-import { initializeSocket } from "./app/socket/socket"; // Import your socket setup
+import { Server } from 'http';
+import app from './app';
+import config from './app/config';
+import { initializeSocket } from './app/socket/socket'; // Import your socket setup
 
 const main = async () => {
   let server: Server;
 
   // 1. Start the HTTP server
   server = app.listen(config.port, () => {
-    console.log(`Session hub server listening on port ${config.port}`);
+    console.log(`Gain server listening on port ${config.port}`);
   });
 
   // 2. Initialize Socket.io by passing the server instance
   initializeSocket(server);
 
   // 3. Optional: Handle process termination
-  process.on("unhandledRejection", (error) => {
-    console.error("Unhandled Rejection:", error);
+  process.on('unhandledRejection', (error) => {
+    console.error('Unhandled Rejection:', error);
     if (server) {
       server.close(() => process.exit(1));
     }
